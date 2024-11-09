ca95e72270a693e097ad0253f56e458990f79838
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-11-10 00:52:26 +0530

    Create Home.js

---
+++ weighitapp/src/screens/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Carousel from '../components/Carousel'
+import Card from '../components/Card'
+
+export default function Home() {
+  return (
+    <div id='home'>
+      <div>
+        <Navbar/>
+      </div>
+      <div >
+        <Carousel/>
+      </div>
+      <div className='fst-bold fs-2 mt-4'><b>OUR PRODUCTS:</b></div>
+      <br/>
+      <div><Card/></div>
+      <div className='m-4'><Footer/></div>
+    </div>
+  )
+}
